0f0b51523d014de25a2223cf1ec91c96764098f9
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-02-14 01:04:27 +0530

    Update viewtutorprofile.blade.php

--- resources/views/student/viewtutorprofile.blade.php
+++ resources/views/student/viewtutorprofile.blade.php
@@ -155,7 +155,7 @@
                 @if(($tutor->user->rating)=='2')
                   <fieldset class="rating">
                     <div class="stars">
-                        <label for="demo-1" aria-label="1 star" title="1 star"></label>
+                        <label for="demo-1" aria-label="1 star" title="2 star"></label>
                         <label for="demo-2" aria-label="2 stars" title="2 stars"></label>
                     </div>
                   </fieldset>
@@ -163,8 +163,8 @@
                 @if(($tutor->user->rating)=='3')
                   <fieldset class="rating">
                     <div class="stars">
-                        <label for="demo-1" aria-label="1 star" title="1 star"></label>
-                        <label for="demo-2" aria-label="2 stars" title="2 stars"></label>
+                        <label for="demo-1" aria-label="1 star" title="3 star"></label>
+                        <label for="demo-2" aria-label="2 stars" title="3 stars"></label>
                         <label for="demo-3" aria-label="3 stars" title="3 stars"></label>
                     </div>
                   </fieldset>
@@ -172,9 +172,9 @@
                 @if(($tutor->user->rating)=='4')
                   <fieldset class="rating">
                     <div class="stars">
-                        <label for="demo-1" aria-label="1 star" title="1 star"></label>
-                        <label for="demo-2" aria-label="2 stars" title="2 stars"></label>
-                        <label for="demo-3" aria-label="3 stars" title="3 stars"></label>
+                        <label for="demo-1" aria-label="1 star" title="4 star"></label>
+                        <label for="demo-2" aria-label="2 stars" title="4 stars"></label>
+                        <label for="demo-3" aria-label="3 stars" title="4 stars"></label>
                         <label for="demo-4" aria-label="4 stars" title="4 stars"></label>   
                     </div>
                   </fieldset>
@@ -182,10 +182,10 @@
                 @if(($tutor->user->rating)=='5')
                   <fieldset class="rating">
                     <div class="stars">
-                        <label for="demo-1" aria-label="1 star" title="1 star"></label>
-                        <label for="demo-2" aria-label="2 stars" title="2 stars"></label>
-                        <label for="demo-3" aria-label="3 stars" title="3 stars"></label>
-                        <label for="demo-4" aria-label="4 stars" title="4 stars"></label>
+                        <label for="demo-1" aria-label="1 star" title="5 star"></label>
+                        <label for="demo-2" aria-label="2 stars" title="5 stars"></label>
+                        <label for="demo-3" aria-label="3 stars" title="5 stars"></label>
+                        <label for="demo-4" aria-label="4 stars" title="5 stars"></label>
                         <label for="demo-5" aria-label="5 stars" title="5 stars"></label>   
                     </div>
                   </fieldset>
